9c1e5fc3b10c612b39235050df32d72f693c3308
Elliot <walker.elliot@elev.ga.ntig.se>
2022-10-27 10:58:20 +0200

    burger

--- SavingGrejs/Program.cs
+++ SavingGrejs/Program.cs
@@ -2,28 +2,61 @@
 using System.Text.Json;
 using System;
 using System.Collections.Generic;
-// string plombaText = File.ReadAllText("plomba.txt");
-// string plombaText2 = File.ReadAllText("plomba2.txt");
-
-// Warrior plomba = JsonSerializer.Deserialize<Warrior>(plombaText);
-// Warrior plomba2 = JsonSerializer.Deserialize<Warrior>(plombaText2);
 
 
-// Console.WriteLine("| Current Settings |");
-// Console.WriteLine("New Settings = 1");
-// Console.WriteLine("Old Settings = 2");
+string plombaText = File.ReadAllText("plomba.txt");
+string plombaText2 = File.ReadAllText("plomba2.txt");
 
-//     int answer;
-
-//     answer = 1;
-
-//     if (answer == 1)
-//     {
-//     Console.WriteLine(plombaText);
-//     }
+Warrior plomba = JsonSerializer.Deserialize<Warrior>(plombaText);
+Warrior plomba2 = JsonSerializer.Deserialize<Warrior>(plombaText2);
 
 
-// Console.ReadLine();
+
+    int ans = 0;
+    bool success = false;
+    
+    while (success != true)
+    {
+        Console.WriteLine("New Settings = 1");
+        Console.WriteLine("Old Settings = 2");
+        string answer = Console.ReadLine();
+
+        success = int.TryParse(answer, out ans);
+    }
+    if (success == true)
+    {
+        if (ans == 1)
+        {
+            Console.WriteLine("\n| Current Settings |\n");
+            Console.WriteLine(plombaText);
+        }
+        if (ans == 2)
+        {
+            Console.WriteLine("\n| Current Settings |\n");
+            Console.WriteLine(plombaText2);
+        }
+        else if (ans > 2 || ans <= 0)
+        {
+            success = false;
+            Console.WriteLine("1 or 2");
+        }
+        
+    }
+
+
+    // if (answer == "1")
+    // {
+    // Console.WriteLine("\n| Current Settings |\n");
+    // Console.WriteLine(plombaText);
+    // }
+
+    // if (answer == "2")
+    // {
+    //     Console.WriteLine("\n| Current Settings |\n");
+    //     Console.WriteLine(plombaText2);
+    // }
+
+Console.ReadLine();
 
 // Console.WriteLine(plomba.Weapon);
 // Console.WriteLine(plomba.Speed);
@@ -31,14 +64,14 @@ using System.Collections.Generic;
 
 
 
-Warrior plomba2 = new Warrior();
+// Warrior plomba2 = new Warrior();
 
-plomba2.Weapon = "";
-plomba2.Stamina = 0;
-plomba2.Speed = 0;
+// plomba2.Weapon = "";
+// plomba2.Stamina = 0;
+// plomba2.Speed = 0;
 
-string plombaText2 = JsonSerializer.Serialize<Warrior>(plomba2);
+// string plombaText2 = JsonSerializer.Serialize<Warrior>(plomba2);
 
-File.WriteAllText("plomba2.txt", plombaText2);
+// File.WriteAllText("plomba2.txt", plombaText2);
 
-Console.ReadLine();
+// Console.ReadLine();
