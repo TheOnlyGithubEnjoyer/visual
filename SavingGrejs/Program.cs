e8dd775279549638058e1442290515416c327a16
Elliot <walker.elliot@elev.ga.ntig.se>
2022-10-27 10:20:16 +0200

    FIxing another text

--- SavingGrejs/Program.cs
+++ SavingGrejs/Program.cs
@@ -1,30 +1,44 @@
 ﻿using System.IO;
 using System.Text.Json;
 using System;
+using System.Collections.Generic;
+// string plombaText = File.ReadAllText("plomba.txt");
+// string plombaText2 = File.ReadAllText("plomba2.txt");
 
-string plombaText = File.ReadAllText("plomba.txt");
-
-Warrior plomba = JsonSerializer.Deserialize<Warrior>(plombaText);
-
-Console.WriteLine("| Current Settings |");
-Console.WriteLine(plombaText);
-
-Console.ReadLine();
+// Warrior plomba = JsonSerializer.Deserialize<Warrior>(plombaText);
+// Warrior plomba2 = JsonSerializer.Deserialize<Warrior>(plombaText2);
 
 
+// Console.WriteLine("| Current Settings |");
+// Console.WriteLine("New Settings = 1");
+// Console.WriteLine("Old Settings = 2");
 
-Console.WriteLine(plomba.Weapon);
-Console.WriteLine(plomba.Speed);
-Console.WriteLine(plomba.Stamina);
+//     int answer;
 
-// Warrior plomba = new Warrior();
+//     answer = 1;
 
-// plomba.Weapon = "";
-// plomba.Stamina = 0;
-// plomba.Speed = 0;
+//     if (answer == 1)
+//     {
+//     Console.WriteLine(plombaText);
+//     }
 
-// string plombaText = JsonSerializer.Serialize<Warrior>(plomba);
 
-// File.WriteAllText("plomba.txt", plombaText);
+// Console.ReadLine();
+
+// Console.WriteLine(plomba.Weapon);
+// Console.WriteLine(plomba.Speed);
+// Console.WriteLine(plomba.Stamina);
+
+
+
+Warrior plomba2 = new Warrior();
+
+plomba2.Weapon = "";
+plomba2.Stamina = 0;
+plomba2.Speed = 0;
+
+string plombaText2 = JsonSerializer.Serialize<Warrior>(plomba2);
+
+File.WriteAllText("plomba2.txt", plombaText2);
 
 Console.ReadLine();
